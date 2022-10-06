b7696eb025cc9e650bc96f06a407e82d172b50c0
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2022-10-07 00:03:03 +0700

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
-const productRouter = require("./app/product/routes");
-const productRouterV2 = require("./app/product_v2/routes")
-// const log = require("./middlewares/logger");
+const productRouter = require("./app/product/routes")
 const path = require("path");
 const logger = require("morgan");
 
 app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/v1', productRouter);
-app.use('/api/v2', productRouterV2);
 app.use((req, res, next) => {
     res.status(404);
     res.send({
--- config/mysql.js
+++ config/mysql.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql");
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_NAME = process.env.DB_NAME || 'latihan_crud_v2';
+const DB_PORT = process.env.DB_PORT || '3306';
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'latihan_crud'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_NAME
 });
 
 module.exports = connection;
